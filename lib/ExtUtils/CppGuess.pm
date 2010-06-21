1775f8b0a3a1816b9dada0d3ef08f482a147dc4c
Mattia Barbon <mattia@develer.com>
2010-06-21 23:58:59 +0200

    Make the tests pass on Linux with GCC.

--- lib/ExtUtils/CppGuess.pm
+++ lib/ExtUtils/CppGuess.pm
@@ -18,4 +18,29 @@ sub new {
     return $self;
 }
 
+sub guess_compiler {
+    my( $self ) = @_;
+    return $self->{guess} if $self->{guess};
+
+    $self->{guess} = { compiler => 'g++',
+                       linker   => 'g++',
+                       };
+
+    return $self->{guess};
+}
+
+sub makemaker_options {
+    my( $self ) = @_;
+    my $g = $self->guess_compiler || die;
+
+    return ( CC => $g->{compiler}, LD => $g->{linker} );
+}
+
+sub module_build_options {
+    my( $self ) = @_;
+    my $g = $self->guess_compiler || die;
+
+    return ( config => { cc => $g->{compiler}, ld => $g->{linker} } );
+}
+
 1;
